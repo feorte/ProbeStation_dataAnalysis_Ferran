f8854c96946e973e752a14143eacce6a0e06a93e
Ferran_personal <ferranort@gmail.com>
2025-04-10 16:39:32 +0200

    read header and table separately

--- main.cpp
+++ main.cpp
@@ -5,17 +5,24 @@
 
 int main()
 {
-    std::ifstream CV_data ("CALICE_6in_256ch_77_20250221_1_CV_customscan.txt");
-    if (!CV_data.is_open()) {
-        cout << "No se pudo abrir el archivo." << endl;
+    std::ifstream CV_data ("CALICE_6in_256ch_77_20250221_1_CV_customscan.txt"); // open the file directly when initializing the stream object
+    if (CV_data.is_open()) {
+        std::string line;
+        
+        // read first the header lines which are not tabular data 
+        for (int i=0; i<28; i++) {
+            std::getline(CV_data,line);
+        }
+
+        // read the tabular data 
+        while (std::getline(CV_data, line)) {
+            std::cout << line << endl;
+        }
     }
-    
-    string line;
-    while (getline(CV_data, line)) {
-        cout << line << endl;
-}
-    
 
+    else {
+        cout << "No se pudo abrir el archivo." << endl;
+    }    
 
-
-}
+    return 0;
+} 
