330a6317914cbb8545e33f248303bcd89eb0a7ea
Ferran_personal <ferranort@gmail.com>
2025-04-20 20:05:42 +0200

    Read data from tree, including vectors

--- analyse_data.cpp
+++ analyse_data.cpp
@@ -7,20 +7,36 @@
 
 int analyse_data()
 {
+
     // read the data in the stored tree
     std::unique_ptr<TFile> data( TFile::Open("stored_data.root") );
-    auto tree = myFile->Get<TTree>("TreeName");
+    auto tree = data->Get<TTree>("analysis");
 
-    int variable;
-    tree->SetBranchAddress("branchName", &variable);
+    // Disable everything...
+    tree->SetBranchStatus("*", false);
+    // ...but the branches we need
+    for (const auto& name : {"voltage", "channel"}) {
+        tree->SetBranchStatus(name, true);
+    }
+    
+    // Associate the branches to variables
+    float voltage;
+    tree->SetBranchAddress("voltage", &voltage);
+
+    std::vector<int>* channel = nullptr;
+    tree->SetBranchAddress("channel", &channel);
+    
+
 
     for (int iEntry = 0; tree->LoadTree(iEntry) >= 0; ++iEntry) {
-    // Load the data for the given tree entry
-    tree->GetEntry(iEntry);
+        // Load the data for the given tree entry
+        tree->GetEntry(iEntry);
 
-    // Now, `variable` is set to the value of the branch
-    // "branchName" in tree entry `iEntry`
-    printf("%d\n", variable);
+        // Now, `variable` is set to the value of the branch
+        // "branchName" in tree entry `iEntry`
+        printf("%f\n", voltage);
+        printf("%d\n", channel->at(1));  // or (*channel)[0]
+
     }
 
 
@@ -28,4 +44,4 @@ int analyse_data()
 
 
     return 0;
-]
+}
