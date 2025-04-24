702ec20f1143c0600e7260cb2c5325c61f811ea5
Ferran Ortiz <feorte@alumni.uv.es>
2025-04-24 13:05:29 +0200

    metido barras de error, comprovar titulos graphs

--- analyse_data.cpp
+++ analyse_data.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector> 
 #include "TGraph.h"
+#include "TGraphErrors.h"
+
 
 
 int analyse_data()
@@ -16,7 +18,7 @@ int analyse_data()
     // Disable everything...
     tree->SetBranchStatus("*", false);
     // ...but the branches we need
-    for (const auto& name : {"voltage", "channel", "cs"}) {
+    for (const auto& name : {"voltage", "channel", "cs", "cs_err"}) {
         tree->SetBranchStatus(name, true);
     }
     
@@ -30,10 +32,15 @@ int analyse_data()
 
     std::vector<float>* cs = nullptr;
     tree->SetBranchAddress("cs", &cs);
+
+    std::vector<float>* cs_err = nullptr;
+    tree->SetBranchAddress("cs_err", &cs_err);
     
     //define variables for the CV graph
     std::vector<float> x;
     std::vector<float> y;
+    std::vector<float> yerr;
+
     int dim; //number of different voltages tested
 
     for (int iEntry = 0; tree->LoadTree(iEntry) >= 0; ++iEntry) {
@@ -43,6 +50,7 @@ int analyse_data()
         //fill vaiables for the CV graph
         x.push_back(voltage);
         y.push_back(cs->at(0));
+        yerr.push_back(cs_err->at(0));
 
         // Now, the associated variables are set to the values of the corresponding branches in tree entry `iEntry`
 
@@ -53,10 +61,22 @@ int analyse_data()
     int ch = channel->at(0); 
     printf("Channel: %d\n", channel->at(0));
     
-    // Create a TGraph object and fill it with the data
-    TGraph *g = new TGraph(dim, &x[0], &y[0]);
-    TString title = Form("Channel %d;X-Axis;Y-Axis", ch);
+    // Create a TGraphErrors object and fill it with the data
+    std::vector<float> xerr(dim, 0); // no error on x-axis
+    TGraph *g = new TGraphErrors(dim, &x[0], &y[0], &xerr[0], &yerr[0]);
+    g->SetName(Form("Channel %d", ch));
+    TString title = Form("Channel %d;Voltage [V]; Capacitance [pF]", ch);
     g->SetTitle(title);
+    g->SetMarkerStyle(20);
+    g->SetMarkerSize(1);
+    g->SetMarkerColor(kBlue);
+    g->SetLineColor(kBlue);
+    g->SetLineWidth(2);
+    g->SetLineStyle(1);
+    g->SetFillColor(kBlue);
+    g->SetFillStyle(3001);
+    g->SetFillColorAlpha(kBlue, 0.1);
+ 
     g->Draw();
     g->GetXaxis()->CenterTitle();
     g->GetYaxis()->CenterTitle();
@@ -64,10 +84,9 @@ int analyse_data()
 
     // Save the graphs
     g->SaveAs("CV_graph.png");
-
     std::unique_ptr<TFile> myFile( TFile::Open("CV_graphs.root", "RECREATE") );
-    TString graph_name = Form("Channel %d", ch);
-    g->Write(graph_name);
+    //TString graph_name = Form("Channel %d", ch);
+    g->Write();
 
 
 
