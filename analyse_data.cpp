6221c46c0ed6538dc6987cd6ec47e6adca657b2b
Ferran Ortiz <feorte@alumni.uv.es>
2025-04-24 11:38:05 +0200

    Created TGraph with CV curve for a given channel

--- analyse_data.cpp
+++ analyse_data.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector> 
+#include "TGraph.h"
 
 
 int analyse_data()
@@ -15,31 +16,44 @@ int analyse_data()
     // Disable everything...
     tree->SetBranchStatus("*", false);
     // ...but the branches we need
-    for (const auto& name : {"voltage", "channel"}) {
+    for (const auto& name : {"voltage", "channel", "cs"}) {
         tree->SetBranchStatus(name, true);
     }
     
+
     // Associate the branches to variables
     float voltage;
     tree->SetBranchAddress("voltage", &voltage);
 
     std::vector<int>* channel = nullptr;
     tree->SetBranchAddress("channel", &channel);
-    
 
+    std::vector<float>* cs = nullptr;
+    tree->SetBranchAddress("cs", &cs);
+    
+    //define variables for the CV graph
+    std::vector<float> x;
+    std::vector<float> y;
+    int dim; //number of different voltages tested
 
     for (int iEntry = 0; tree->LoadTree(iEntry) >= 0; ++iEntry) {
         // Load the data for the given tree entry
         tree->GetEntry(iEntry);
 
-        // Now, `variable` is set to the value of the branch
-        // "branchName" in tree entry `iEntry`
-        printf("%f\n", voltage);
-        printf("%d\n", channel->at(1));  // or (*channel)[0]
+        //fill vaiables for the CV graph
+        x.push_back(voltage);
+        y.push_back(cs->at(0));
+
+        // Now, the associated variables are set to the values of the corresponding branches in tree entry `iEntry`
+
+        dim=iEntry+1;
 
     }
 
+    printf("Channel: %d\n", channel->at(0));
 
+    TGraph *g = new TGraph(dim, &x[0], &y[0]);
+    g->Draw("AL");
 
 
 
